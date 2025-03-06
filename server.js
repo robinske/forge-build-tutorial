ee1dee3677a6f3919be6f78d31fb6d9cd00c1d28
Kelley R <krobinson@twilio.com>
2025-03-06 14:32:50 -0500

    Step 1 - can echo back messages on wss

--- server.js
+++ server.js
@@ -6,85 +6,30 @@ wss.on('connection', (ws) => {
 
   // Listener for call information and conversation
   ws.on('message', (data) => {
-
     let message = JSON.parse(data);
-    console.log("Received message type: ", message.type);
-    console.log("Parsed message", message);
 
     switch (message.type) {
         case 'setup':
-            console.log('Setup');
+            console.log(`Conversation setup initiated for number ending in **${message.from.slice(-2)}`);
+            console.log(`Call SID: ${message.callSid}`);
             break;
         case 'prompt':
             console.log('Prompt: ', message.voicePrompt);
 
-            // TODO - this triggers { type: 'error', description: 'Invalid message received: {"message":"Echo this back."}' }
-            // TODO - figure out MVP for echoing back messages
+            // echo back the voice prompt
             ws.send(JSON.stringify({
                 type: 'text',
                 token: message.voicePrompt,
-                last: true,
+                last: false,
               }));
-            break;
-        case 'interrupt':
-            console.log('Interrupt');
             break;
         case 'error':
             console.log('Error');
-            break;
-        case 'dtmf':
-            console.log('DTMF');
             break;
         default:
             console.log('Unknown message type');
             break;
     }
-
-    
-    // copied from Hao's post
-    // ----------------------- //
-    // console.log(msg);
-    // if (msg.type === 'setup') {
-    //     addLog('convrelay', `convrelay socket setup ${msg.callSid}`);
-    //     callSid = msg.callSid;        
-    //     gptService.setCallInfo('user phone number', msg.from);
-
-    //     //trigger gpt to start 
-    //     gptService.completion('hello', interactionCount);
-    //     interactionCount += 1;
-
-    //     if(record.recording){
-    //     recordingService(textService, callSid).then(() => {
-    //         console.log(`Twilio -> Starting recording for ${callSid}`.underline.red);
-    //     });
-    //     }
-    // }  
-    
-    // if (msg.type === 'prompt') {
-    //     addLog('convrelay', `convrelay -> GPT (${msg.lang}) :  ${msg.voicePrompt} `);
-    //     gptService.completion(msg.voicePrompt, interactionCount);
-    //     interactionCount += 1;
-    // } 
-    
-    // if (msg.type === 'interrupt') {
-    //     addLog('convrelay', 'convrelay interrupt: utteranceUntilInterrupt: ' + msg.utteranceUntilInterrupt + ' durationUntilInterruptMs: ' + msg.durationUntilInterruptMs);
-    //     gptService.interrupt();
-    //     // console.log('Todo: add interruption handling');
-    // }
-
-    // if (msg.type === 'error') {
-    //     addLog('convrelay', 'convrelay error: ' + msg.description);
-        
-    //     console.log('Todo: add error handling');
-    // }
-
-    // if (msg.type === 'dtmf') {
-    //     addLog('convrelay', 'convrelay dtmf: ' + msg.digit);
-        
-    //     console.log('Todo: add dtmf handling');
-    // }
-    // ----------------------- //
-
   });
 
   // Event listener for client disconnection
